6692df27da5ab6a3b8c8993c972be9051bc4001a
Sivaranjan Karthikeyan <SivaranjanK+lumel@lumel.com>
2025-06-06 09:22:43 +0530

    DB Updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -11,13 +11,22 @@ const { LeaveApproval } = require('../entities/LeaveApproval');
 dotenv.config();
 
 // Create a TypeORM data source instance
+// const AppDataSource = new DataSource({
+//   type: 'mysql',
+//   host: process.env.DB_HOST,
+//   port: parseInt(process.env.DB_PORT || '3306'),
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   synchronize:true,
+//   logging: false, 
+//   entities: [User,Role, LeaveType,LeavePolicy,LeaveApproval, LeaveBalance, LeaveRequest],
+//   migrations: ['src/migrations/**/*.ts'],
+// });
+
 const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '3306'),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url:process.env.DB_CONNECTION_STRING,
   synchronize:false,
   logging: false, 
   entities: [User,Role, LeaveType,LeavePolicy,LeaveApproval, LeaveBalance, LeaveRequest],
