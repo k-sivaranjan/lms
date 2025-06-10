a70b498635754deba81e4a9830afedda010ad43c
Sivaranjan Karthikeyan <SivaranjanK+lumel@lumel.com>
2025-06-10 18:00:36 +0530

    Fixed Bugs Worker

--- backend/config/db.js
+++ backend/config/db.js
@@ -11,22 +11,14 @@ const { LeaveApproval } = require('../entities/LeaveApproval');
 dotenv.config();
 
 // Create a TypeORM data source instance
-// const AppDataSource = new DataSource({
-//   type: 'mysql',
-//   host: process.env.DB_HOST,
-//   port: parseInt(process.env.DB_PORT || '3306'),
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   synchronize:true,
-//   logging: false, 
-//   entities: [User,Role, LeaveType,LeavePolicy,LeaveApproval, LeaveBalance, LeaveRequest],
-//   migrations: ['src/migrations/**/*.ts'],
-// });
-
 const AppDataSource = new DataSource({
   type: 'mysql',
-  url:process.env.DB_CONNECTION_STRING,
+  url:process.env.DB_CONNECTION_STRING, 
+  // host: process.env.DB_HOST,
+  // port: parseInt(process.env.DB_PORT || '3306'),
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
   synchronize:false,
   logging: false, 
   entities: [User,Role, LeaveType,LeavePolicy,LeaveApproval, LeaveBalance, LeaveRequest],
--- backend/workers/userBulkWorker.js
+++ backend/workers/userBulkWorker.js
@@ -3,26 +3,31 @@ const Queue = require('bull');
 const { AppDataSource } = require('../config/db');
 const { createUser } = require('../models/userModel');
 
-const userQueue = new Queue('userQueue', {
-  redis: { port: process.env.REDIS_PORT, host: process.env.REDIS_HOST,password:process.env.REDIS_PASSWORD } 
-});
+const redisConfig = {
+  port: process.env.REDIS_PORT || 6379,
+  host: process.env.REDIS_HOST || 'localhost',
+  ...(process.env.REDIS_PASSWORD && { password: process.env.REDIS_PASSWORD })
+};
+
+const userQueue = new Queue('userQueue', { redis: redisConfig });
 
 (async () => {
   try {
     await AppDataSource.initialize();
-    
-    userQueue.process(async (job, done) => {
+    userQueue.process(async (job) => {
       const { users } = job.data;
       
-      try {
-        for (const user of users) {
+      for (const user of users) {
+        try {
           await createUser(user);
+          console.log(`Created user ${user.email || user.username}`);
+        } catch (userError) {
+          console.error(`Error creating user:`, user);
         }
-        done();
-      } catch (err) {
-        done(err);
       }
     });
+
+    console.log('Worker is now listening for jobs...');
     
   } catch (err) {
     process.exit(1);
